b2d6850440966ea1eb37902515171d1476a765e1
veresoora <lahanabira@gmail.com>
2023-03-30 10:44:36 +0700

    membuat integer array, comparison, data movements, and n

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
 using namespace std;
+
+//array of intergers to hold values
+int arr[20];
+int cmp_count = 0; // number of comparision
+int mov_count = 0; // number of data movements 
+int n;
