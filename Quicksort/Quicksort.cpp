9fd78f2eee2384578397e2c82a49ef2dfccdbdd9
veresoora <lahanabira@gmail.com>
2023-03-30 11:51:24 +0700

    Fungsi untuk memulai program dengan memanggil beberapa fungsi

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -110,3 +110,16 @@ void display() {
 	cout << "\n\nNumber of comparasion: " << cmp_count << endl;
 	cout << "Number of data movements: " << mov_count << endl;
 }
+
+
+int main()
+{
+
+	input();
+	//sort array using quick sort
+	q_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
+}
